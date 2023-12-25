a151011254b202129a4947ca50de1d2142bbdf64
priosshrsth <shrsthprios@hotmail.com>
2023-12-25 06:46:50 +0000

    seeder

--- app/Models/Comment.ts
+++ app/Models/Comment.ts
@@ -7,21 +7,24 @@ export default class Comment extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
-
   @column()
   public user_id: number
 
   @column()
   public thread_id: number
 
+  @column()
+  public content: string
+
   @belongsTo(() => User, { foreignKey: 'user_id' })
   public user: BelongsTo<typeof User>
 
   @belongsTo(() => Thread, { foreignKey: 'thread_id' })
   public thread: BelongsTo<typeof Thread>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
 }
