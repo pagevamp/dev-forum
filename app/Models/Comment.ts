27d22cd09a251f065371511c09ff5c7ea2223205
priosshrsth <shrsthprios@hotmail.com>
2023-12-25 06:13:25 +0000

    comments modal

--- app/Models/Comment.ts
+++ app/Models/Comment.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Thread from './Thread'
 
 export default class Comment extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +12,16 @@ export default class Comment extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @column()
+  public user_id: number
+
+  @column()
+  public thread_id: number
+
+  @belongsTo(() => User, { foreignKey: 'user_id' })
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Thread, { foreignKey: 'thread_id' })
+  public thread: BelongsTo<typeof Thread>
 }
